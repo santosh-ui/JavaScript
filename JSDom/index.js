3efeadba55e0e8da936337394001da7f46f436ce
santosh-ui <rajusantosh@gmail.com>
2021-04-28 21:24:25 +0530

    learning dom

---
+++ JSDom/index.js
@@ -0,0 +1,9 @@
+alert("Hello! Welcome to DOM Manipulation Program.");
+
+document.querySelector("a").style.color = "red";
+document.querySelector("#checkbox").click();
+
+let heading = document.querySelector("#title");
+heading.innerHTML = "Good Bye!"
+
+// document.querySelectorAll("a").style.color = "black";
